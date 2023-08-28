928d6be2597dad771fe4af73335187a1050e206b
Christian Miguez <kisquian@gmail.com>
2023-08-28 19:51:00 -0300

    a

--- src/pages/api/whatsapp.ts
+++ src/pages/api/whatsapp.ts
@@ -1,3 +1,11 @@
+export const config = {
+  runtime: "edge",
+};
+
+export default (request: Request) => {
+  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
+};
+
 export async function get() {
   return {
     body: JSON.stringify({
